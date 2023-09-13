17983cd17ed68d7c99129d6e0c555b6be4a0d844
Sara Fryzlewicz <sfryzlewicz@hawk.iit.edu>
2023-09-12 20:07:30 -0500

    Initial Commit

---
+++ FetchSomeRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchSomeRecipes
+//
+//  Created by Sara Fryzlewicz on 9/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
